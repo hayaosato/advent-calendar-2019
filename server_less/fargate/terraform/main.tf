585054c486a639ee2695442bbc63b76c8d386290
hayaosato <jake.bibikyary.880@gmail.com>
2019-12-17 09:09:31 +0900

    add terraform

--- server_less/fargate/terraform/main.tf
+++ server_less/fargate/terraform/main.tf
@@ -1,3 +1,48 @@
+resource "aws_iam_role" "default" {
+  name               = var.service_name
+  description        = "IAM Rolw for ${var.run_task}"
+  assume_role_policy = file("${var.run_task}-role.json")
+}
+
+resource "aws_iam_policy" "default" {
+  name        = var.service_name
+  description = "IAM Policy for ${var.run_task}"
+  policy      = file("${var.run_task}-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
+}
+
+resource "aws_cloudwatch_log_group" "default" {
+  name              = "/aws/lambda/${var.run_task}"
+  retention_in_days = 7
+}
+
+data archive_file "default" {
+  type        = "zip"
+  source_dir  = "../${var.run_task}"
+  output_path = "${var.run_task}.zip"
+}
+
+resource "aws_lambda_function" "default" {
+  filename         = "${var.run_task}.zip"
+  function_name    = var.run_task
+  role             = aws_iam_role.default.arn
+  handler          = "main.main"
+  source_code_hash = data.archive_file.default.output_base64sha256
+  runtime          = "python3.7"
+  environment {
+    variables = {
+      CLUSTER_NAME      = var.service_name
+      SUBNET_ID         = var.subnet_id
+      SECURITY_GROUP_ID = var.security_group_id
+    }
+  }
+}
+
+
 resource "aws_ecs_cluster" "default" {
   name = var.service_name
 }
--- server_less/fargate/terraform/vairables.tf
+++ server_less/fargate/terraform/vairables.tf
@@ -1,3 +1,11 @@
 variable "service_name" {
   default = "hogehoge"
 }
+
+variable "run_task" {
+  default = "run-task-function"
+}
+
+variable "subnet_id" {}
+
+variable "security_group_id" {}
